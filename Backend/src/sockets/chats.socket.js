94f607e92f7d3f41091f359b43d1cb123160c18c
dhruvdiwakar01 <dhruvdiwakar.1@gmail.com>
2026-03-21 22:57:55 +0530

    final edit

--- Backend/src/sockets/chats.socket.js
+++ Backend/src/sockets/chats.socket.js
@@ -5,7 +5,7 @@ let socket = null
 export const initializeSocketConnection = () => {
   if (socket && socket.connected) return socket
 
-  socket = io('http://localhost:3000', {
+  socket = io(import.meta.env.VITE_API_URL || 'http://localhost:3000', {
     withCredentials: true,
     reconnectionAttempts: 3,
     reconnectionDelay: 2000,
